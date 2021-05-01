d521ace4354d694bfd86c80fdf4295e559a5b12c
Otavio Monteagudo <oivatom@gmail.com>
2021-05-01 17:54:53 -0300

    implemented dice roll with dice images

--- DiceRoller/app/src/main/java/otamm/github/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/otamm/github/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package otamm.github.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        val diceValue: TextView = findViewById<TextView>(R.id.dice_value_text)
+        val diceImage: ImageView = findViewById<ImageView>(R.id.dice_image)
         rollButton.setOnClickListener {
-            val randomInt: String = rollDice().toString()
-            diceValue.text = randomInt
+            rollDice(diceImage)
         }
     }
 
-    private fun rollDice(): Int {
+    private fun rollDice(diceImage: ImageView) {
         val randomInt: Int = Random().nextInt(6) + 1
-        return randomInt
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
