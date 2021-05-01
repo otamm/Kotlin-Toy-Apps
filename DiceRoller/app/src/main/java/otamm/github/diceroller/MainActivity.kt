e7d3254bae9281a95672e836489b273c40e70f09
Otavio Monteagudo <oivatom@gmail.com>
2021-05-01 06:01:10 -0300

    generating random numbers on app screen

--- DiceRoller/app/src/main/java/otamm/github/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/otamm/github/diceroller/MainActivity.kt
@@ -3,16 +3,23 @@ package otamm.github.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        val diceValue: TextView = findViewById<TextView>(R.id.dice_value_text)
         rollButton.setOnClickListener {
-            rollButton.text = "AAAAA"
-            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+            val randomInt: String = rollDice().toString()
+            diceValue.text = randomInt
         }
+    }
+
+    private fun rollDice(): Int {
+        val randomInt: Int = Random().nextInt(6) + 1
+        return randomInt
     }
 }
